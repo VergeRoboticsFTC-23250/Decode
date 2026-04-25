5ce35b27b3c1cdb7f653cdca1574067a275efc7f
vivekmaddineni <vivekmaddineni@gmail.com>
2026-04-25 01:26:22 -0500

    updated pedro folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/pedro/Tuning.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.lib.pedro;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawCurrentAndHistory;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.changes;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.drawCurrent;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.drawCurrentAndHistory;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.follower;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.stopRobot;
+import static org.firstinspires.ftc.teamcode.lib.pedro.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
